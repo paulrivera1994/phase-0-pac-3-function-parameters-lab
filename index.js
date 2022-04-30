d9faf433b2b22bad99145071d1c9fb2035583e16
Paul Rivera <juniorrivera81@yahoo.com>
2022-04-30 00:38:06 -0600

    commit tree 4d0c2e6d95e1281aa2cf7cc14eb0205d17816cb5

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction(Aki) {
+    console.log(`Hi, my name is ${Aki}.`);
+    return `Hi, my name is ${Aki}.`
+}
+function introductionWithLanguage(Aki, Ember) {
+    return `Hi, my name is ${Aki} and I am learning to program in ${Ember}.`
+}
+function introductionWithLanguageOptional(Aki, Ember.js) {
+    console.log(Aki, Ember.js);
+}
